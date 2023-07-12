5dec46c8d050cf82312460aa6eeca8ab17db0bcf
KakashiHatake27 <trialacc.akshdeep@gmail.com>
2023-07-11 20:46:51 -0400

    Up to Product display and ui

---
+++ ecommerce/lib/client.js
@@ -0,0 +1,15 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: '2023-07-04',
+    useCdn: true,
+    token: process.env.NEXT_PUBLIC_SANITY_PUBLIC_API_TOKEN
+});
+
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
